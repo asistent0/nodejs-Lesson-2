ba73935b979f3a77119dfdc50c378d6b8209a4e0
asistent <asistent0@gmail.com>
2016-05-04 23:34:06 +0300

    Разделил логику на несколько файлов В функцию очистки добавил поддержку длинного флага

---
+++ clear.js
@@ -0,0 +1,11 @@
+/**
+ * Created by asistent on 04.05.2016.
+ */
+var fs = require('fs');
+fs.unlink('log.txt', function(err) {
+    if (err) {
+        console.log('Ещё не сыграно ни одной игры');
+    } else {
+        console.log('Файл успешно очищен');
+    }
+});
--- index.js
+++ index.js
@@ -1,59 +1,11 @@
 var readline = require('readline');
-var fs = require("fs");
+var fs = require('fs');
 var args = require('minimist')(process.argv.slice(2));
 
 if (args._[0] === 'log.txt') {
-    fs.readFile('log.txt', 'utf8', function (err, data) {
-        if (err) {
-            console.log('Ещё не сыграно ни одной игры');
-            return;
-        }
-
-        var arr = data.split('\n');
-        var parties = arr.length - 1;
-
-        var win = 0, loss = 0, maxWin = 0, maxLoss = 0, i = 0, j = 0;
-
-        for (var key in arr) {
-            if (arr[key] === 'Win') {
-                win++;
-                i++;
-            } else {
-                if (i >= maxWin) {
-                    maxWin = i;
-                    i = 0;
-                }
-                i = 0;
-            }
-
-            if (arr[key] === 'Loss') {
-                loss++;
-                j++;
-            } else {
-                if (j >= maxLoss) {
-                    maxLoss = j;
-                    j = 0;
-                }
-                j = 0;
-            }
-
-        }
-
-        console.log('Сыграно партий ' + parties);
-        console.log('Выигранных партий ' + win);
-        console.log('Проигранных партий ' + loss);
-        console.log('Соотношение партий: ' + win + ':' + loss);
-        console.log("Максимальне число побед подряд = " + maxWin);
-        console.log("Максимальне число проигрышей подряд = " + maxLoss);
-    });
-} else if (args.c === true) {
-    fs.unlink('log.txt', function(err) {
-        if (err) {
-            console.log('Ещё не сыграно ни одной игры');
-        } else {
-            console.log('Файл успешно очищен');
-        }
-    });
+    var stat = require('./stat');
+} else if ((args.c === true) || (args.clear === true)) {
+    var clear = require('./clear');
 } else {
     var rl = readline.createInterface({
         input: process.stdin,
