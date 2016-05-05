301939cb2f1c3bce7e6231b829b3aeac1c758f86
asistent <asistent0@gmail.com>
2016-05-05 19:41:21 +0300

    Добавил возможность играть несколько игр подряд

--- index.js
+++ index.js
@@ -5,28 +5,34 @@ var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
 var sideCoin = [["1", "Орел"], ["2", "Решка"]];
 
-var rand = Math.floor(Math.random() * sideCoin.length);
-rl.write('Введите 1 (Орел) или 2 (Решка):\n');
+function start() {
+    console.log('Введите 1 (Орел), 2 (Решка) или q (выход):');
+}
+
+start();
 
 rl.on('line', function (cmd) {
-    this.write('Вы выбрали "' + cmd + '" ');
+    var rand = Math.floor(Math.random() * sideCoin.length);
 
-    var string;
-    if (cmd === sideCoin[rand][0]) {
-        this.write('Урааа! Вы выиграли!');
-        string = "Win\n";
+    if (cmd === 'q') {
+        this.close();
     } else {
-        this.write('Упс. Вы проиграли.');
-        string = "Loss\n";
-    }
-    fs.appendFile('log.txt', string, function (err) {
-        if (err) {
-            throw err;
+        console.log('Вы выбрали "' + cmd + '", Компьютер выбрал "' + sideCoin[rand][0] + '"');
+        var string;
+        if (cmd === sideCoin[rand][0]) {
+            console.log('Урааа! Вы выиграли!');
+            string = "Win\n";
+        } else {
+            console.log('Упс. Вы проиграли.');
+            string = "Loss\n";
         }
-    });
-    this.close();
+        start();
+        fs.appendFile('log.txt', string, function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+    }
 });
-
