e925457f43433ead7368507f742931938dc0bcc7
asistent <asistent0@gmail.com>
2016-05-05 18:43:04 +0300

    ещё сильнее разделил логику программы, теперь каждый файл не знает о существовании другого

--- clear.js
+++ clear.js
@@ -2,10 +2,14 @@
  * Created by asistent on 04.05.2016.
  */
 var fs = require('fs');
-fs.unlink('log.txt', function(err) {
-    if (err) {
-        console.log('Ещё не сыграно ни одной игры');
-    } else {
-        console.log('Файл успешно очищен');
-    }
-});
+var args = require('minimist')(process.argv.slice(2));
+
+if (args._[0] === 'log.txt') {
+    fs.unlink('log.txt', function (err) {
+        if (err) {
+            console.log('Ещё не сыграно ни одной игры');
+        } else {
+            console.log('Файл успешно очищен');
+        }
+    });
+}
--- index.js
+++ index.js
@@ -1,39 +1,32 @@
 var readline = require('readline');
 var fs = require('fs');
-var args = require('minimist')(process.argv.slice(2));
 
-if (args._[0] === 'log.txt') {
-    var stat = require('./stat');
-} else if ((args.c === true) || (args.clear === true)) {
-    var clear = require('./clear');
-} else {
-    var rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    var sideCoin = [["1", "Орел"], ["2", "Решка"]];
+var sideCoin = [["1", "Орел"], ["2", "Решка"]];
 
-    var rand = Math.floor(Math.random() * sideCoin.length);
+var rand = Math.floor(Math.random() * sideCoin.length);
+rl.write('Введите 1 (Орел) или 2 (Решка):\n');
 
-    rl.write('Введите 1 (Орел) или 2 (Решка):\n');
+rl.on('line', function (cmd) {
+    this.write('Вы выбрали "' + cmd + '" ');
 
-    rl.on('line', function (cmd) {
-        this.write('Вы выбрали "' + cmd + '" ');
-
-        var string;
-        if (cmd === sideCoin[rand][0]) {
-            this.write('Урааа! Вы выиграли!');
-            string = "Win\n";
-        } else {
-            this.write('Упс. Вы проиграли.');
-            string = "Loss\n";
+    var string;
+    if (cmd === sideCoin[rand][0]) {
+        this.write('Урааа! Вы выиграли!');
+        string = "Win\n";
+    } else {
+        this.write('Упс. Вы проиграли.');
+        string = "Loss\n";
+    }
+    fs.appendFile('log.txt', string, function (err) {
+        if (err) {
+            throw err;
         }
-        fs.appendFile('log.txt', string, function (err) {
-            if (err) {
-                throw err;
-            }
-        });
-        this.close();
     });
-}
+    this.close();
+});
+
